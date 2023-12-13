93a509de60367cdd0a575bd7b53afe3851e5832f
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-12-13 17:43:57 -0500

    fixed links on portfolio page

--- src/components/UI/Project.jsx
+++ src/components/UI/Project.jsx
@@ -7,14 +7,15 @@ export default function ProjectCard({ project }) {
                 {project.map((proj) => (
                     <div key={proj.id} className="card text-bg-dark project">
                         <li>
-                            <a href={proj.link}>
+                            <Link to={proj.link}>
                                 <img src={proj.image} className="card-img" alt={proj.altText}></img>
-                            </a>
                                 <div className="card-img-overlay">
                                 <h5 className="card-title">{proj.title}</h5>
                                 <p className="card-text">MERN Stack</p>
+                                <a href={proj.github}>Github Link</a>
                                 </div>
-                            <Link to={proj.github}>Github Link</Link>
+                            </Link>
+                            
                         </li>
                     </div>
                 ))
