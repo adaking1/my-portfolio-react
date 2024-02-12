7f27d8c19e4a46a0c0c587b902c17f659fc42db8
adaking1 <137830553+adaking1@users.noreply.github.com>
2024-02-12 10:14:29 -0500

    edited about me

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -12,7 +12,7 @@ export default function AboutMe() {
                 Coming out of college with a music degree, my options were limited. After many years of
                 working various jobs, I found a passion for coding. At the beginning of 2022 I started a Python certificate that I completed by the end of that April.
                 That program helped blossom a love for coding and using very logical thinking. Since then I have completed a fullstack web deveopment bootcamp through the University of Connecticut, 
-                where I learned to code using front-end and back-end technologies such as MongoDB, Express.js, React.js, Node.js, and MySQL. These skills are exhibitied in the applications found in the "My Work" page, and this portfolio itself.
+                where I learned to code using front-end and back-end technologies such as MongoDB, Express.js, React.js, Node.js, and MySQL. These skills are exhibitied in the applications found in the "Portfolio" page, as well as this site itself.
             </p>
         </div>
         </>
